6cafafe7bab46230510b767f8cf7b1348968b509
hasnuan <hasnuan83@gmail.com>
2026-05-19 15:04:54 +0700

    create navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,14 @@
+
+import "./navbar.css"
+
+export default function Navbar(){
+    return(
+        <nav className="navbar">
+            <ul className="nav-ui">
+                <li className="nav-item">Home</li>
+                <li className="nav-item">About</li>
+                <li className="nav-item">Contact</li>
+            </ul>
+        </nav>
+    )
+}
