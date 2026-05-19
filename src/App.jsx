bb98eba3d14916cfbda404a5f93393c3f43f5526
hasnuan <hasnuan83@gmail.com>
2026-05-19 11:20:24 +0700

    add button popup

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,10 @@ function App() {
       <h1>Count:{count}</h1>
       <button onClick={()=>setCount(count + 1)}>Tambah</button>
       <button onClick={()=>setCount(count - 1)}>Kurang</button>
+
+      <div>
+        <button onClick={()=> alert("Tombol ditekan!")}>Tampilkan Pop Up</button>
+      </div>
     </div>
   )
 }
