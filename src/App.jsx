f21d40dcdaac81099eb7342d2322c55682e85f5b
hasnuan <hasnuan83@gmail.com>
2026-05-19 10:51:04 +0700

    add feature count

--- src/App.jsx
+++ src/App.jsx
@@ -2,5 +2,15 @@ import { useState } from "react";
 
 function App() {
 
-  const [count, setCount] = useState
+  const [count, setCount] = useState(0)
+  
+  return (
+    <div>
+      <h1>Count:{count}</h1>
+      <button onClick={()=>setCount(count + 1)}>Tambah</button>
+      <button onClick={()=>setCount(count - 1)}>Kurang</button>
+    </div>
+  )
 }
+
+export default App
